5f382d48981cfd7511225e646ad8d6b55f166bf5
iii5412 <iii5412@gmail.com>
2022-03-07 20:13:59 +0900

    repository로 부터 자원과 권한정보가져와 Map객체로 매핑 후 반환하는 Service

---
+++ src/main/java/com/security/corespringsecurity5/security/service/SecurityResourceService.java
@@ -0,0 +1,49 @@
+package com.security.corespringsecurity5.security.service;
+
+import com.security.corespringsecurity5.domain.entity.Resources;
+import com.security.corespringsecurity5.domain.entity.Role;
+import com.security.corespringsecurity5.repository.ResourcesRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 데이터 계층으로부터 자원 정보, 권한정보를 가져와 Map객체로 매핑한다.
+ */
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class SecurityResourceService {
+
+    final private ResourcesRepository resourcesRepository;
+
+    /*
+    public void setResourcesRepository(ResourcesRepository resourcesRepository) {
+        this.resourcesRepository = resourcesRepository;
+    }
+    */
+
+    public LinkedHashMap<RequestMatcher, List<ConfigAttribute>> getResourceList() {
+
+        LinkedHashMap<RequestMatcher, List<ConfigAttribute>> result = new LinkedHashMap<>();
+        final List<Resources> resourcesList = resourcesRepository.findAllResources();
+        resourcesList.forEach(resource -> {
+            List<ConfigAttribute> configAttributeList = new ArrayList<>();
+            resource.getRoleSet().forEach(role -> {
+                configAttributeList.add(new SecurityConfig(role.getRoleName()));
+            });
+            result.put(new AntPathRequestMatcher(resource.getResourceName()), configAttributeList);
+        });
+        return result;
+    }
+}
