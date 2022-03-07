dd61af69b682f57fd41ccbf32f8f7b9b1bdf63c1
iii5412 <iii5412@gmail.com>
2022-03-07 20:21:43 +0900

    ModelMapper Bean등록

---
+++ src/main/java/com/security/corespringsecurity5/config/AppConfig.java
@@ -0,0 +1,18 @@
+package com.security.corespringsecurity5.config;
+
+import com.security.corespringsecurity5.repository.ResourcesRepository;
+import com.security.corespringsecurity5.security.service.SecurityResourceService;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration
+class AppConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
