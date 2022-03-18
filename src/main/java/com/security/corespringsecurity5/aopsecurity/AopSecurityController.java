3f7dc577fec397ddc988a8bd95febdbfb89a3211
iii5412 <iii5412@gmail.com>
2022-03-18 14:08:14 +0900

    메소드 방식 preAuthorize 진행중

---
+++ src/main/java/com/security/corespringsecurity5/aopsecurity/AopSecurityController.java
@@ -0,0 +1,25 @@
+package com.security.corespringsecurity5.aopsecurity;
+
+import com.security.corespringsecurity5.domain.dto.AccountDto;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class AopSecurityController {
+
+    @GetMapping("/preAuthorize")
+    @PreAuthorize("hasRole('ROLE_USER') and #accountDto.username == principal.username")
+    public String preAuthorize(AccountDto accountDto, Model model, Principal principal) {
+
+        model.addAttribute("method", "Success @PreAuthorize");
+
+        return "/aop/method";
+    }
+
+}
--- src/main/java/com/security/corespringsecurity5/security/configs/SecurityConfig.java
+++ src/main/java/com/security/corespringsecurity5/security/configs/SecurityConfig.java
@@ -29,6 +29,7 @@ import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -49,6 +50,7 @@ import java.util.List;
 
 @EnableWebSecurity
 @Order(1)
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
